cd0db90823f91e7d1a4c3284e93fa287142802f5
jefe23 <124675057+jefe23@users.noreply.github.com>
2023-08-04 14:12:31 +0200

    Best KGE value so far...

--- settings/6_Norway_1.bat
+++ settings/6_Norway_1.bat
@@ -1,2 +1,2 @@
-python C:\Users\jefe\Documents\GitHub\CWatM\run_cwatm.py settings_norway_template.ini -l
+python C:\Users\jefe\Documents\GitHub\CWatM\run_cwatm.py settings_norway_template_Copy.ini -l
 pause
