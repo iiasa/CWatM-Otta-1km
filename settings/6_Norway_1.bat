a5a92de369fc9d6c17f3ca56aed1d574fae02497
jefe23 <124675057+jefe23@users.noreply.github.com>
2024-07-05 11:09:26 +0200

    Crop files, Calibration_JuneV2 settings file and Otta observations for reservoir paper
    Crop files, so no double counting grassland, 50/50 land use sealed/grassland; Calibration JuneV2 settings file and Otta observations for reservoir paper tables

--- settings/6_Norway_1.bat
+++ settings/6_Norway_1.bat
@@ -1,2 +1,2 @@
-python C:\Users\jefe\Documents\GitHub\CWatM\run_cwatm.py settings_norway_template_All_dvlps_medianv3Tranf.ini -l
+python C:\Users\jefe\Documents\GitHub\CWatM\run_cwatm.py settings_norway_calibration_June_v2-Run21_best_crops_SMoutputs_withGlaciers.ini -l
 pause
