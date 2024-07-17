cbe7880581e6649120c4c442ca897ccdc0ce0195
jefe23 <124675057+jefe23@users.noreply.github.com>
2024-07-17 11:35:59 +0200

    new calibration parameters in settings file
    new JuneV2 calibration parameters in settings file and remove Otta observations and reservoir paper results from Otta folder

--- settings/6_Norway_1.bat
+++ settings/6_Norway_1.bat
@@ -1,2 +1,2 @@
-python C:\Users\jefe\Documents\GitHub\CWatM\run_cwatm.py settings_norway_calibration_June_v2-Run21_best_crops_SMoutputs_withGlaciers.ini -l
+python C:\Users\jefe\Documents\GitHub\CWatM\run_cwatm.py settings_norway_calibration_June_v2-Best_5yr_crops.ini -l
 pause
